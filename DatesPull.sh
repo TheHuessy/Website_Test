01abfc5c090e102d75c83b173e6bc9ca48afb552
TheHuessy <jameshuessy@gmail.com>
2019-08-19 07:45:17 -0400

    'almost'

--- DatesPull.sh
+++ DatesPull.sh
@@ -4,4 +4,5 @@
 #!/usr/bin/git
 
 /usr/bin/python3 /home/pi/Documents/Website_Test/DatesPull.py
+cd /home/pi/Documents/Website_Test
 /usr/bin/git status > /home/pi/Documents/Website_Test/ETL.log 2>&1
