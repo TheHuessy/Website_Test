ad58091a6958170f352e38a48e96d60b4affab0a
TheHuessy <jameshuessy@gmail.com>
2019-08-19 07:34:36 -0400

    'etl'

--- DatesPull.sh
+++ DatesPull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #!/bin/bash
 #!/usr/bin/python3
+#!/usr/bin/git
 
 /usr/bin/python3 /home/pi/Documents/Website_Test/DatesPull.py
+/usr/bin/git status > /home/pi/Documents/Website_Test/ETL.log
