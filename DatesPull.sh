0dddbf52f24c5ee2870838cdd46d258ac108ca9a
TheHuessy <jameshuessy@gmail.com>
2019-08-19 20:43:20 -0400

    'log_output'

--- DatesPull.sh
+++ DatesPull.sh
@@ -9,7 +9,12 @@ DATE = `date`
 cd /home/pi/Documents/Website_Test
 /usr/bin/python3 DatesPull.py > /home/pi/Documents/Website_Test/ETL.log 2>&1
 echo --------------------- >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo PULLING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+/usr/bin/git pull >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo ADDING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 /usr/bin/git add . >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo COMMITTING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 /usr/bin/git commit -m 'auto_commit_$DATE' >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo PUSHING >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 /usr/bin/git push >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 echo ===================== >> /home/pi/Documents/Website_Test/ETL.log 2>&1
