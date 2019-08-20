8c43417857715d1b545105656fb69b9eb0ba1910
TheHuessy <jameshuessy@gmail.com>
2019-08-19 22:35:40 -0400

    'hello'

--- DatesPull.sh
+++ DatesPull.sh
@@ -17,5 +17,6 @@ sudo /usr/bin/git add . >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 echo COMMITTING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 sudo /usr/bin/git commit -m 'auto_commit_$DATE' >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 echo PUSHING >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo /usr/bin/git config --global user.name >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 sudo /usr/bin/git push origin master >> /home/pi/Documents/Website_Test/ETL.log 2>&1
 echo ===================== >> /home/pi/Documents/Website_Test/ETL.log 2>&1
