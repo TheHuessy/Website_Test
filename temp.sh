1c1bd15845ba46292a8e1daf0c346d3e932c1a63
TheHuessy <jameshuessy@gmail.com>
2019-09-11 07:18:49 -0400

    't3'

--- temp.sh
+++ temp.sh
@@ -4,9 +4,10 @@
 #!/usr/bin/git
 
 
-DATE=$(date +"%m%d%Y")
-DATE > /home/pi/Documents/Website_Test/ETL.log 2>&1
+DATE_T=$(date +"%m%d%Y")
 cd /home/pi/Documents/Website_Test
+echo "$DATE_T" > /home/pi/Documents/Website_Test/ETL.log 2>&1
+
 ls > /home/pi/Documents/Website_Test/ETL.log 2>&1
 # echo $DATE > /home/pi/Documents/Website_Test/ETL.log 2>&1
 # sudo /usr/bin/python3 DatesPull.py >> /home/pi/Documents/Website_Test/ETL.log 2>&1
