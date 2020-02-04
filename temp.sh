06e029246a2c9f1aae4703d197ea4b9c81c37abd
TheHuessy <jameshuessy@gmail.com>
2020-02-03 22:18:02 -0500

    auto_commit_

--- temp.sh
+++ temp.sh
@@ -9,16 +9,21 @@ cd /home/pi/Documents/Website_Test
 sudo echo "$DATE_T" > /home/pi/Documents/Website_Test/ETL.log 2>&1
 
 # ls >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo $DATE > /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo $DATE > /home/pi/Documents/Website_Test/ETL.log 2>&1
 # sudo /usr/bin/python3 DatesPull.py >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo --------------------- >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo PULLING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# sudo /usr/bin/git pull >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo ADDING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# sudo /usr/bin/git add . >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo COMMITTING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# sudo /usr/bin/git commit -m 'auto_commit_$DATE' >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo PUSHING >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo /usr/bin/git config --global user.name >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo --------------------- >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo PULLING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+# sudo -l /usr/bin/git pull >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+
+
+/bin/bash -c "git status" >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+su -s /bin/bash pi -c "git pull" >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo ADDING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+su -s /bin/bash pi -c "git add ."
+echo COMMITTING... >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+su -s /bin/bash pi -c "git config --global user.name " >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+su -s /bin/bash pi -c "git commit -m 'auto_commit_$DATE'" >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo PUSHING >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+# su -s /bin/bash pi -c "git push"
 # sudo /usr/bin/git push origin master >> /home/pi/Documents/Website_Test/ETL.log 2>&1
-# echo ===================== >> /home/pi/Documents/Website_Test/ETL.log 2>&1
+echo ===================== >> /home/pi/Documents/Website_Test/ETL.log 2>&1
